c443cff24ba4a7dc8c22ebdf86667509c275192e
QiuQiuking <3416413327@qq.com>
2025-08-24 09:37:04 +0800

    更新 config.py

--- config.py
+++ config.py
@@ -9,4 +9,4 @@ CHANNEL_ID = int(os.getenv("CHANNEL_ID", "-1"))
 GROUP_ID = int(os.getenv("GROUP_ID", "-1"))
 CHANNEL_URL = os.getenv("CHANNEL_URL")
 GROUP_URL = os.getenv("GROUP_URL")
-POLL_INTERVAL = int(os.getenv("POLL_INTERVAL", "10"))
+POLL_INTERVAL = int(os.getenv("POLL_INTERVAL", 30))  # 默认 30 秒
